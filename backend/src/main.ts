1e93f13bd58633d5ecff534885dddd686c435472
Vedran Hrncic <vedran.hrncic@gmail.com>
2026-02-02 13:22:43 +0100

    Update CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
-    origin: 'https://prudential-universal-launchpad-fron.vercel.app',
+    origin: [
+      'http://localhost:5173',
+      'https://prudential-universal-launchpad-fron.vercel.app',
+    ],
     credentials: true,
   });
   await app.listen(3000);
